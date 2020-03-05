7bb9d250839a8c5daf521b87f84d73a8c5fbcdd9
Marcel Friedrichs <mfriedrichs@techfak.uni-bielefeld.de>
2020-03-05 12:19:23 +0100

    #10: Add edges from association to MED-RT concepts

--- src/biodwh2-medrt/src/main/java/de/unibi/agbi/biodwh2/medrt/etl/MEDRTGraphExporter.java
+++ src/biodwh2-medrt/src/main/java/de/unibi/agbi/biodwh2/medrt/etl/MEDRTGraphExporter.java
@@ -8,6 +8,8 @@ import de.unibi.agbi.biodwh2.core.model.graph.Node;
 import de.unibi.agbi.biodwh2.medrt.MEDRTDataSource;
 import de.unibi.agbi.biodwh2.medrt.model.*;
 
+import java.util.Locale;
+
 public class MEDRTGraphExporter extends GraphExporter<MEDRTDataSource> {
     @Override
     protected Graph exportGraph(MEDRTDataSource dataSource) throws ExporterException {
@@ -41,7 +43,7 @@ public class MEDRTGraphExporter extends GraphExporter<MEDRTDataSource> {
     private void addTerms(Graph g, Node terminologyNode, Terminology terminology) throws ExporterException {
         for (Term term : terminology.terms) {
             Node termNode = createNodeFromModel(g, term);
-            g.addEdge(new Edge(terminologyNode, termNode, "HAS_TERM"));
+            g.addEdge(new Edge(termNode, terminologyNode, "IN_TERMINOLOGY"));
             g.addEdge(new Edge(termNode, g.findNode("Namespace", "name", term.namespace), "IN_NAMESPACE"));
         }
     }
@@ -49,33 +51,54 @@ public class MEDRTGraphExporter extends GraphExporter<MEDRTDataSource> {
     private void addConcepts(Graph g, Node terminologyNode, Terminology terminology) throws ExporterException {
         for (Concept concept : terminology.concepts) {
             Node conceptNode = createNodeFromModel(g, concept);
-            for (Property property : concept.properties) {
-                Node propertyNode = createNodeFromModel(g, property);
-                g.addEdge(new Edge(conceptNode, propertyNode, "HAS_PROPERTY"));
-                g.addEdge(new Edge(propertyNode, g.findNode("Namespace", "name", property.namespace), "IN_NAMESPACE"));
-            }
-            for (Synonym synonym : concept.synonyms) {
-                Node termNode = g.findNode("Term", "name", synonym.toName);
-                Edge e = new Edge(conceptNode, termNode, "HAS_SYNONYM");
-                e.setProperty("name", synonym.name);
-                e.setProperty("preferred", synonym.preferred);
-                g.addEdge(e);
-            }
-            g.addEdge(new Edge(terminologyNode, conceptNode, "HAS_CONCEPT"));
+            addConceptProperties(g, concept, conceptNode);
+            addConceptSynonyms(g, concept, conceptNode);
+            g.addEdge(new Edge(conceptNode, terminologyNode, "IN_TERMINOLOGY"));
             g.addEdge(new Edge(conceptNode, g.findNode("Namespace", "name", concept.namespace), "IN_NAMESPACE"));
+        }
+    }
+
+    private void addConceptProperties(final Graph g, final Concept concept,
+                                      final Node conceptNode) throws ExporterException {
+        for (Property property : concept.properties) {
+            Node propertyNode = createNodeFromModel(g, property);
+            g.addEdge(new Edge(conceptNode, propertyNode, "HAS_PROPERTY"));
+            g.addEdge(new Edge(propertyNode, g.findNode("Namespace", "name", property.namespace), "IN_NAMESPACE"));
+        }
+    }
+
+    private void addConceptSynonyms(final Graph g, final Concept concept, final Node conceptNode) {
+        for (Synonym synonym : concept.synonyms) {
+            Node termNode = g.findNode("Term", "name", synonym.toName);
+            Edge e = new Edge(conceptNode, termNode, "HAS_SYNONYM");
+            e.setProperty("name", synonym.name);
+            e.setProperty("preferred", synonym.preferred);
+            g.addEdge(e);
         }
     }
 
     private void addAssociations(Graph g, Node terminologyNode, Terminology terminology) throws ExporterException {
         for (Association association : terminology.associations) {
             Node associationNode = createNodeFromModel(g, association);
-            Node qualifierNode = createNodeFromModel(g, association.qualifier);
-            g.addEdge(new Edge(associationNode, qualifierNode, "HAS_QUALIFIER"));
-            g.addEdge(new Edge(qualifierNode, g.findNode("Namespace", "name", association.qualifier.namespace),
-                               "IN_NAMESPACE"));
-            g.addEdge(new Edge(terminologyNode, associationNode, "HAS_ASSOCIATION"));
+            associationNode.setProperty(association.qualifier.name.toLowerCase(Locale.US), association.qualifier.value);
+            connectAssociationToConcepts(g, association, associationNode);
+            g.addEdge(new Edge(associationNode, terminologyNode, "IN_TERMINOLOGY"));
             g.addEdge(
                     new Edge(associationNode, g.findNode("Namespace", "name", association.namespace), "IN_NAMESPACE"));
+        }
+    }
+
+    private void connectAssociationToConcepts(final Graph g, final Association association,
+                                              final Node associationNode) {
+        if (association.fromNamespace.equals("MED-RT")) {
+            Node fromNode = g.findNode("Concept", "code", association.fromCode);
+            if (fromNode != null)
+                g.addEdge(new Edge(fromNode, associationNode, "HAS_ASSOCIATION"));
+        }
+        if (association.toNamespace.equals("MED-RT")) {
+            Node toNode = g.findNode("Concept", "code", association.toCode);
+            if (toNode != null)
+                g.addEdge(new Edge(associationNode, toNode, "ASSOCIATED_WITH"));
         }
     }
 }
