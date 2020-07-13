30901efedc82d2574fcd3af300053556f91ee268
Marcel Friedrichs <mfriedrichs@techfak.uni-bielefeld.de>
2020-07-13 07:48:37 +0200

    #26 Implement MONDO updater and start work on MONDO graph exporter

--- src/biodwh2-mondo/src/main/java/de/unibi/agbi/biodwh2/mondo/etl/MondoGraphExporter.java
+++ src/biodwh2-mondo/src/main/java/de/unibi/agbi/biodwh2/mondo/etl/MondoGraphExporter.java
@@ -3,13 +3,38 @@ package de.unibi.agbi.biodwh2.mondo.etl;
 import de.unibi.agbi.biodwh2.core.Workspace;
 import de.unibi.agbi.biodwh2.core.etl.GraphExporter;
 import de.unibi.agbi.biodwh2.core.exceptions.ExporterException;
+import de.unibi.agbi.biodwh2.core.exceptions.ExporterFormatException;
+import de.unibi.agbi.biodwh2.core.io.obo.OboEntry;
+import de.unibi.agbi.biodwh2.core.io.obo.OboReader;
 import de.unibi.agbi.biodwh2.core.model.graph.Graph;
+import de.unibi.agbi.biodwh2.core.model.graph.Node;
 import de.unibi.agbi.biodwh2.mondo.MondoDataSource;
+
+import java.io.IOException;
 
 public class MondoGraphExporter extends GraphExporter<MondoDataSource> {
     @Override
     protected boolean exportGraph(final Workspace workspace, final MondoDataSource dataSource,
                                   final Graph graph) throws ExporterException {
+        graph.setNodeIndexPropertyKeys("id");
+        try {
+            OboReader reader = new OboReader(dataSource.resolveSourceFilePath(workspace, "mondo.obo"), "UTF-8");
+            for (OboEntry entry : reader)
+                if (entry.getName().equals("Term"))
+                    exportEntry(graph, entry);
+        } catch (IOException e) {
+            throw new ExporterFormatException("Failed to export mondo.obo", e);
+        }
         return true;
+    }
+
+    private void exportEntry(final Graph graph, final OboEntry entry) {
+        if (entry.containsKey("is_obsolete") && "true".equalsIgnoreCase(entry.getFirst("is_obsolete")))
+            return;
+        Node node = createNode(graph, "Term");
+        node.setProperty("id", entry.getFirst("id"));
+        node.setProperty("name", entry.getFirst("name"));
+        // TODO: more properties and relationships
+        graph.update(node);
     }
 }
