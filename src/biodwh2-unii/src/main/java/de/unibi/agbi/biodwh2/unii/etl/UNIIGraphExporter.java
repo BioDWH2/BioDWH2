cda2f339326ee5f74d5bbf2785f9693cfda7fc73
Marcel Friedrichs <astror.enales@gmail.com>
2020-12-09 16:18:19 +0100

    #29 Prevent UNII graph exporter crash with missing data entry

--- src/biodwh2-unii/src/main/java/de/unibi/agbi/biodwh2/unii/etl/UNIIGraphExporter.java
+++ src/biodwh2-unii/src/main/java/de/unibi/agbi/biodwh2/unii/etl/UNIIGraphExporter.java
@@ -36,13 +36,17 @@ public class UNIIGraphExporter extends GraphExporter<UNIIDataSource> {
                 uniiEntriesMap.put(entry.unii, new ArrayList<>());
             uniiEntriesMap.get(entry.unii).add(entry);
         }
-        for (String unii : uniiEntriesMap.keySet())
-            createUNIINode(graph, uniiEntriesMap.get(unii), dataSource.uniiDataEntries.get(unii));
+        for (Map.Entry<String, List<UNIIEntry>> entry : uniiEntriesMap.entrySet())
+            createUNIINode(graph, entry.getValue(), dataSource.uniiDataEntries.get(entry.getKey()));
         return true;
     }
 
     private void createUNIINode(final Graph graph, final List<UNIIEntry> entries, final UNIIDataEntry dataEntry) {
-        Node uniiNode = createNodeFromModel(graph, dataEntry);
+        Node uniiNode;
+        if (dataEntry == null)
+            uniiNode = graph.addNode("UNII", "id", entries.get(0).unii);
+        else
+            uniiNode = createNodeFromModel(graph, dataEntry);
         uniiNode.setProperty("name", entries.get(0).displayName);
         uniiNode.setProperty("official_names", getNameArrayOfTypeFromEntries(entries, "of"));
         uniiNode.setProperty("systematic_names", getNameArrayOfTypeFromEntries(entries, "sys"));
