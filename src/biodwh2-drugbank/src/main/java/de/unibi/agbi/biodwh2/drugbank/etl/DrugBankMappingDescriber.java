ff5f443a1547643803f5d281dfb131127b5c4e8c
TDietr <57195768+TDietr@users.noreply.github.com>
2021-03-26 09:23:08 +0100

    FinishedDrugbankMappingDescriber

--- src/biodwh2-drugbank/src/main/java/de/unibi/agbi/biodwh2/drugbank/etl/DrugBankMappingDescriber.java
+++ src/biodwh2-drugbank/src/main/java/de/unibi/agbi/biodwh2/drugbank/etl/DrugBankMappingDescriber.java
@@ -12,96 +12,98 @@ public class DrugBankMappingDescriber extends MappingDescriber {
     }
 
     @Override
-    public NodeMappingDescription[] describe(Graph graph, Node node, String localMappingLabel) {
-        if ("Drug".equals(localMappingLabel)) {
-            NodeMappingDescription description = new NodeMappingDescription(NodeMappingDescription.NodeType.DRUG);
-            description.addIdentifier(IdentifierType.DRUG_BANK, node.<String>getProperty("drugbank_id"));
-            return new NodeMappingDescription[]{description};
-        }
-        if ("Pathway".equals(localMappingLabel)) {
-            NodeMappingDescription description = new NodeMappingDescription(NodeMappingDescription.NodeType.PATHWAY);
-            description.addIdentifier(IdentifierType.SMPDB, node.<String>getProperty("smpdbId"));
-            return new NodeMappingDescription[]{description};
-        }
-        /*
-        if("Chemical Property".equals(localMappingLabel)){
-            NodeMappingDescription description = new NodeMappingDescription(NodeMappingDescription.NodeType.COMPOUND);
-            description.addIdentifier(IdentifierType.CAS, node.getProperty("cas-number"));
-            return new NodeMappingDescription[]{description};
-        }*/
-        if ("Salt".equals(localMappingLabel)) {
-            NodeMappingDescription description = new NodeMappingDescription(NodeMappingDescription.NodeType.COMPOUND);
-            description.addIdentifier(IdentifierType.CAS, node.<String>getProperty("cas-number"));
-            description.addIdentifier(IdentifierType.UNII, node.<String>getProperty("unii"));
-            return new NodeMappingDescription[]{description};
-        }
-        /*
-        if("External Identifier")){
+    public NodeMappingDescription[] describe(final Graph graph, final Node node, final String localMappingLabel) {
+        if ("Drug".equalsIgnoreCase(localMappingLabel))
+            return describeDrug(node);
+        if ("Pathway".equalsIgnoreCase(localMappingLabel))
+            return describePathway(node);
+        if ("Drug".equalsIgnoreCase(localMappingLabel))
             return describeCompound(node);
-        }*/
-        if ("Polypeptide".equals(localMappingLabel)) {
-            return describeProtein(node, graph);
-        }
-        if ("Article".equals(localMappingLabel)) {
-            NodeMappingDescription description = new NodeMappingDescription(
-                    NodeMappingDescription.NodeType.PUBLICATION);
-            description.addIdentifier(IdentifierType.PUBMED_ID, node.<String>getProperty("pubmedId"));
-            return new NodeMappingDescription[]{description};
-        }
+        if ("Salt".equalsIgnoreCase(localMappingLabel))
+            return describeCompound(node);
+        if("Polypeptide".equalsIgnoreCase(localMappingLabel))
+            return new NodeMappingDescription[]{describeGene(graph, node), describeProtein(graph, node)};
+        if ("Article".equalsIgnoreCase(localMappingLabel))
+            return describeArticle(node);
+        if("Organism".equalsIgnoreCase(localMappingLabel))
+            return describeOrganism(node);
         return null;
     }
+
+    private NodeMappingDescription[] describeDrug(final Node node){
+        final NodeMappingDescription description = new NodeMappingDescription(NodeMappingDescription.NodeType.DRUG);
+        description.addIdentifier(IdentifierType.DRUG_BANK, node.<String>getProperty("drugbank_id"));
+        description.addName(node.getProperty("name"));
+        return new NodeMappingDescription[]{description};
+        }
+
+    private NodeMappingDescription[] describePathway(final Node node){
+        final NodeMappingDescription description = new NodeMappingDescription(NodeMappingDescription.NodeType.PATHWAY);
+        description.addIdentifier(IdentifierType.SMPDB, node.<String>getProperty("smpdb_id"));
+        description.addName(node.getProperty("name"));
+        return new NodeMappingDescription[]{description};
+        }
 
     private NodeMappingDescription[] describeCompound(final Node node) {
-        NodeMappingDescription description = new NodeMappingDescription(NodeMappingDescription.NodeType.COMPOUND);
-        if (node.getProperty("resource").equals("ChemSpider")) {
-            description.addIdentifier(IdentifierType.CHEMSPIDER, node.<String>getProperty("identifier"));
-            return new NodeMappingDescription[]{description};
-        }
-        if (node.getProperty("resource").equals("PharmGKB")) {
-            description.addIdentifier(IdentifierType.PHARM_GKB, node.<String>getProperty("identifier"));
-            return new NodeMappingDescription[]{description};
-        }
-        if (node.getProperty("resource").equals("PubChem Compound")) {
-            description.addIdentifier(IdentifierType.PUB_CHEM_COMPOUND, node.<String>getProperty("identifier"));
-            return new NodeMappingDescription[]{description};
-        }
-
-        return null;
-    }
-
-    private NodeMappingDescription[] describeProtein(final Node node, Graph graph) {
-        NodeMappingDescription description = new NodeMappingDescription(NodeMappingDescription.NodeType.PROTEIN);
-        Long[] nodeIds = graph.getAdjacentNodeIdsForEdgeLabel(node.getProperty("__id"),
-                                                              "DrugBank_HAS_POLYPEPTIDE_EXTERNAL_IDENTIFIER");
-        for (int i = 0; i < nodeIds.length; i++) {
-            Node adjacentNode = graph.getNode(nodeIds[i]);
-            if (adjacentNode.getProperty("resource").equals("UniProtKB")) {
-                description.addIdentifier(IdentifierType.UNIPROT_KB, adjacentNode.<String>getProperty("identifier"));
-            } else if (adjacentNode.getProperty("resource").equals("UniProt Accession")) {
-                description.addIdentifier(IdentifierType.UNIPROT_KB, adjacentNode.<String>getProperty("identifier"));
-            }
-        }
+        final NodeMappingDescription description = new NodeMappingDescription(NodeMappingDescription.NodeType.COMPOUND);
+        description.addIdentifier(IdentifierType.CAS, node.<String>getProperty("cas_number"));
+        description.addIdentifier(IdentifierType.UNII, node.<String>getProperty("unii"));
+        description.addName(node.getProperty("name"));
         return new NodeMappingDescription[]{description};
     }
 
-    private NodeMappingDescription[] describeGene(final Node node, Graph graph) {
-        NodeMappingDescription description = new NodeMappingDescription(NodeMappingDescription.NodeType.GENE);
-        Long[] nodeIds = graph.getAdjacentNodeIdsForEdgeLabel(node.getProperty("__id"),
-                                                              "DrugBank_HAS_POLYPEPTIDE_EXTERNAL_IDENTIFIER");
+    private NodeMappingDescription describeProtein(final Graph graph, final Node node) {
+        final NodeMappingDescription description = new NodeMappingDescription(NodeMappingDescription.NodeType.PROTEIN);
+        Long[] nodeIds = graph.getAdjacentNodeIdsForEdgeLabel(node.getId(),
+                                                              "DrugBank_HAS_EXTERNAL_IDENTIFIER");
+        description.addName(node.getProperty("name"));
+            for (int i = 0; i < nodeIds.length; i++) {
+                Node adjacentNode = graph.getNode(nodeIds[i]);
+                if (adjacentNode.getProperty("resource").equals("UniProtKB")) {
+                    description.addIdentifier(IdentifierType.UNIPROT_KB, adjacentNode.<String>getProperty("id"));
+                } else if (adjacentNode.getProperty("resource").equals("UniProt Accession")) {
+                    description.addIdentifier(IdentifierType.UNIPROT_KB, adjacentNode.<String>getProperty("id"));
+                }
+
+            }
+        return description;
+    }
+
+    private NodeMappingDescription describeGene(final Graph graph, final Node node) {
+        final NodeMappingDescription description = new NodeMappingDescription(NodeMappingDescription.NodeType.GENE);
+        Long[] nodeIds = graph.getAdjacentNodeIdsForEdgeLabel(node.getId(),
+                                                              "DrugBank_HAS_EXTERNAL_IDENTIFIER");
+        description.addName(node.getProperty("gene_name"));
         for (int i = 0; i < nodeIds.length; i++) {
             Node adjacentNode = graph.getNode(nodeIds[i]);
+            String hgncLabel = adjacentNode.getProperty("id");
+            String correctedLabel = hgncLabel.replaceFirst("HGNC:", ""); 
             if (adjacentNode.getProperty("resource").equals("HUGO Gene Nomenclature Committee (HGNC)")) {
-                description.addIdentifier(IdentifierType.HGNC_ID, adjacentNode.<String>getProperty("identifier"));
+                description.addIdentifier(IdentifierType.HGNC_ID, correctedLabel);
             } else if (adjacentNode.getProperty("resource").equals("GenAtlas")) {
-                description.addIdentifier(IdentifierType.GEN_ATLAS, adjacentNode.<String>getProperty("identifier"));
+                description.addIdentifier(IdentifierType.GEN_ATLAS, adjacentNode.<String>getProperty("id"));
             }
         }
+        return description;
+    }
+
+    private NodeMappingDescription[] describeArticle(final Node node){
+        final NodeMappingDescription description = new NodeMappingDescription(NodeMappingDescription.NodeType.PUBLICATION);
+        description.addIdentifier(IdentifierType.PUBMED_ID, node.<String>getProperty("pubmed_id"));
+        description.addName(node.getProperty("citation"));
+        return new NodeMappingDescription[]{description};
+    }
+
+    private NodeMappingDescription[] describeOrganism(final Node node){
+        final NodeMappingDescription description = new NodeMappingDescription(NodeMappingDescription.NodeType.TAXON);
+        description.addIdentifier(IdentifierType.NCBI_TAXON, node.<String>getProperty("id"));
+        description.addName(node.getProperty("name"));
         return new NodeMappingDescription[]{description};
     }
 
     @Override
     protected String[] getNodeMappingLabels() {
-        return new String[]{"Drug", "Pathway", "Chemical Property", "Polypeptide", "Salt", "Article"};
+        return new String[]{"Drug", "Pathway", "Salt", "Polypeptide",  "Article", "Organism"};
     }
 
     @Override
