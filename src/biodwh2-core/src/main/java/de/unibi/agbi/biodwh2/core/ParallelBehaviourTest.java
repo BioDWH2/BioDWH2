e34699b8659ae6cfa94785d7096929985c499778
Anna Fritzen <fritzenanna101@gmail.com>
2021-09-23 19:02:54 +0200

    test class now measures average execution time for both parallel and sequential approach, added cmd line arguments for control

--- src/biodwh2-core/src/main/java/de/unibi/agbi/biodwh2/core/ParallelBehaviourTest.java
+++ src/biodwh2-core/src/main/java/de/unibi/agbi/biodwh2/core/ParallelBehaviourTest.java
@@ -5,78 +5,103 @@ import java.io.IOException;
 import java.text.DecimalFormat;
 import java.util.stream.Stream;
 
+import picocli.CommandLine;
+
 import org.apache.commons.io.FileUtils;
 
-import de.unibi.agbi.biodwh2.core.exceptions.DataSourceException;
-
+/**
+ * Test class for measuring parallel vs sequential execution of data source processing.
+ */
 public class ParallelBehaviourTest {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		System.out.println("--- TESTING PARALLEL BEHAVIOUR ---");
-		// mock workspace and data sources
-		Workspace workspace = new Workspace("");
-		final String[] ids = { "Mock1", "Mock2", "Mock3" };
-		final DataSource[] datasources = new DataSourceLoader().getDataSources(ids);
-		
-		DecimalFormat df = new DecimalFormat();
-		df.setMaximumFractionDigits(2);
-		
-		// => SEQUENTIAL
-		long start = System.currentTimeMillis();
-		for(DataSource ds : datasources) {
-			try {
-				ds.prepare(workspace);
-			} catch (DataSourceException e) {
-				e.printStackTrace();
-			}
-			ds.parse(workspace);
-			ds.export(workspace);
-		}
-		long stop = System.currentTimeMillis();
-		long elapsed = stop - start;
-		System.out.println("=> SEQUENTIAL TIME TAKEN: " + elapsed + " ms ( " + df.format((float) elapsed / 1000) + " SECONDS)");
-		
-		// => PARALLEL
-		start = System.currentTimeMillis();
-		Stream.of(datasources).parallel().forEach(
-				ds -> {
-					try {
-						ds.prepare(workspace);
-					} catch (DataSourceException e) {
-						e.printStackTrace();
-					}
-					ds.parse(workspace);
-					ds.export(workspace);				
-				});
-		stop = System.currentTimeMillis();
-		elapsed = stop - start;
-		System.out.println("=> PARALLEL TIME TAKEN: " + elapsed + " ms (" + df.format((float) elapsed / 1000) + " SECONDS)");
+        System.out.println("--- TESTING PARALLEL BEHAVIOUR ---\n");
 
-		// cleanup ...
-		try {
-			FileUtils.forceDelete(new File("./sources/"));
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		
-	}
+        // parse cmd line args (not yet implemented in this case ...)
+        final CmdArgsParallelTest commandLine = parseCommandLine(args);
+
+        // mock workspace and data sources
+        Workspace workspace = new Workspace("");
+        final String[] ids = { "Mock1", "Mock2", "Mock3" };
+        final DataSource[] datasourcesS, datasourcesP;
+
+        // prepare output formatting
+        DecimalFormat df = new DecimalFormat();
+        df.setMaximumFractionDigits(2);
+
+        int numIterations = 1;
+        long start, stop, elapsed, averageExecutionTime;
+
+        if(commandLine.numIterations > 0) {
+            numIterations = commandLine.numIterations;
+        }
+
+        if(commandLine.parallel) {
+
+            // => PARALLEL
+            datasourcesP = new DataSourceLoader().getDataSources(ids);
+            start = System.currentTimeMillis();
+
+            for(int i = 0; i < numIterations; i++) {
+
+                Stream.of(datasourcesP).parallel().forEach(
+                        ds -> {
+                            try {
+                                ds.prepare(workspace);
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                            }
+                            ds.parse(workspace);
+                            ds.export(workspace);
+                        });
+            }
+
+            stop = System.currentTimeMillis();
+            elapsed = stop - start;
+            averageExecutionTime = elapsed/numIterations;
+            System.out.println("AVG PARALLEL EXECUTION TIME: " + averageExecutionTime + " MS (" + df.format((float) averageExecutionTime) + " SECONDS), " + numIterations + " ITERATIONS");
+
+        } else {
+
+            // => SEQUENTIAL
+            datasourcesS = new DataSourceLoader().getDataSources(ids);
+            start = System.currentTimeMillis();
+
+            for(int i = 0; i < numIterations; i++) {
+
+                for(DataSource ds : datasourcesS) {
+                    try {
+                        ds.prepare(workspace);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                    ds.parse(workspace);
+                    ds.export(workspace);
+                }
+            }
+
+            stop = System.currentTimeMillis();
+            elapsed = stop - start;
+            averageExecutionTime = elapsed/numIterations;
+            System.out.println("AVG SEQUENTIAL EXECUTION TIME: " + averageExecutionTime + " MS (" + df.format((float) averageExecutionTime) + " SECONDS), " + numIterations + " ITERATIONS");
+        }
+
+        // cleanup ...
+        try {
+            FileUtils.forceDelete(new File("./sources/"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static CmdArgsParallelTest parseCommandLine(final String... args) {
+        final CmdArgsParallelTest result = new CmdArgsParallelTest();
+        final CommandLine cmd = new CommandLine(result);
+        cmd.setPosixClusteredShortOptionsAllowed(true);
+        cmd.parseArgs(args);
+        return result;
+    }
 
 
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
 }
