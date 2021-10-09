4016bfb61125589f72b8aac0c48e363d20f02a8a
Anna Fritzen <fritzenanna101@gmail.com>
2021-10-09 19:24:19 +0200

    added test case for custom pool, tests are disabled

--- src/biodwh2-core/src/test/java/de/unibi/agbi/biodwh2/core/WorkspaceTest.java
+++ src/biodwh2-core/src/test/java/de/unibi/agbi/biodwh2/core/WorkspaceTest.java
@@ -2,16 +2,23 @@ package de.unibi.agbi.biodwh2.core;
 
 import org.apache.commons.io.FileUtils;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.io.File;
 import java.io.IOException;
 import java.text.DecimalFormat;
+import java.util.concurrent.ForkJoinPool;
 import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
+@Disabled("For development and demonstration purposes only")
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class WorkspaceTest {
 
@@ -27,13 +34,9 @@ class WorkspaceTest {
         df.setMaximumFractionDigits(2);
     }
 
-    @Test
-    void processDataSourcesInParallel() {;
-
-        final DataSource[] datasourcesS, datasourcesP;
-
-        int numIterations = 5;
-        boolean parallel = true;
+    @ParameterizedTest
+    @MethodSource("parallelProvider")
+    void processDataSourcesInParallel(boolean isParallel, int numIterations) {
 
         long start, stop, elapsed, averageExecutionTime;
 
@@ -41,15 +44,15 @@ class WorkspaceTest {
             numIterations = numIterations;
         }
 
-        if (parallel) {
+        if (isParallel) {
 
             // => PARALLEL
-            datasourcesP = new DataSourceLoader().getDataSources(ids);
+            final DataSource[] datasources = new DataSourceLoader().getDataSources(ids);
             start = System.currentTimeMillis();
 
             for (int i = 0; i < numIterations; i++) {
 
-                Stream.of(datasourcesP).parallel().forEach(ds -> {
+                Stream.of(datasources).parallel().forEach(ds -> {
                     try {
                         ds.prepare(workspace);
                     } catch (Exception e) {
@@ -69,12 +72,12 @@ class WorkspaceTest {
         } else {
 
             // => SEQUENTIAL
-            datasourcesS = new DataSourceLoader().getDataSources(ids);
+            final DataSource[] datasources = new DataSourceLoader().getDataSources(ids);
             start = System.currentTimeMillis();
 
             for (int i = 0; i < numIterations; i++) {
 
-                for (DataSource ds : datasourcesS) {
+                for (DataSource ds : datasources) {
                     try {
                         ds.prepare(workspace);
                     } catch (Exception e) {
@@ -101,4 +104,77 @@ class WorkspaceTest {
 
     }
 
+    @ParameterizedTest
+    @MethodSource("parallelPoolProvider")
+    void processDataSourcesInParallelWithPool(int numThreads) {
+
+        final DataSource[] datasources = new DataSourceLoader().getDataSources(ids);
+        ForkJoinPool customPool = null;
+        long start, stop, elapsed;
+
+        try {
+
+            System.out.println("Creating new custom thread pool with " + numThreads + " threads ... ");
+            // init new pool and add task to parse and export each datasource
+            customPool = new ForkJoinPool(numThreads);
+            start = System.currentTimeMillis();
+            customPool.submit(() -> Stream.of(datasources).parallel().forEach(ds -> {
+
+                try {
+                    ds.prepare(workspace);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                ds.parse(workspace);
+                ds.export(workspace);
+            })).get();
+
+            stop = System.currentTimeMillis();
+            elapsed = stop - start;
+            System.out.println("=> TIME TAKEN " + (elapsed) + " MS (" + df.format(elapsed / 1000) + " SECONDS)");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (customPool != null) {
+                System.out.println("Shutting down custom pool ...");
+                customPool.shutdown();
+            }
+        }
+
+    }
+
+    static Stream<Arguments> parallelProvider() {
+        return Stream.of(arguments(true, 10), arguments(false, 10));
+    }
+
+    static Stream<Arguments> parallelPoolProvider() {
+        return Stream.of(arguments(2), arguments(4), arguments(6), arguments(Runtime.getRuntime().availableProcessors()));
+    }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
