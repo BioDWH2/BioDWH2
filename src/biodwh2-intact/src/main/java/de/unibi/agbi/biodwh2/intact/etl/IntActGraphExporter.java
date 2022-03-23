237c5e7203257ac40176d1ddd275d085caaca2ef
Marcel Friedrichs <astror.enales@gmail.com>
2022-03-23 11:05:16 +0100

    Update IntActGraphExporter.java

--- src/biodwh2-intact/src/main/java/de/unibi/agbi/biodwh2/intact/etl/IntActGraphExporter.java
+++ src/biodwh2-intact/src/main/java/de/unibi/agbi/biodwh2/intact/etl/IntActGraphExporter.java
@@ -1,20 +1,26 @@
 package de.unibi.agbi.biodwh2.intact.etl;
 
-import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 import de.unibi.agbi.biodwh2.core.Workspace;
 import de.unibi.agbi.biodwh2.core.etl.GraphExporter;
 import de.unibi.agbi.biodwh2.core.exceptions.ExporterException;
 import de.unibi.agbi.biodwh2.core.exceptions.ExporterFormatException;
 import de.unibi.agbi.biodwh2.core.io.FileUtils;
-import de.unibi.agbi.biodwh2.core.io.mixml.EntrySet;
+import de.unibi.agbi.biodwh2.core.io.mixml.*;
+import de.unibi.agbi.biodwh2.core.model.DataSourceFileType;
 import de.unibi.agbi.biodwh2.core.model.graph.Graph;
+import de.unibi.agbi.biodwh2.core.model.graph.IndexDescription;
+import de.unibi.agbi.biodwh2.core.model.graph.Node;
 import de.unibi.agbi.biodwh2.intact.IntActDataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.*;
+import java.nio.file.Path;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 
@@ -32,8 +38,8 @@ public class IntActGraphExporter extends GraphExporter<IntActDataSource> {
 
     @Override
     protected boolean exportGraph(final Workspace workspace, final Graph graph) throws ExporterException {
+        graph.addIndex(IndexDescription.forNode("BioSource", "ncbi_tax_id", false, IndexDescription.Type.UNIQUE));
         exportEntrySets(workspace, graph);
-        System.exit(0);
         return true;
     }
 
@@ -46,12 +52,12 @@ public class IntActGraphExporter extends GraphExporter<IntActDataSource> {
             final ZipInputStream stream = FileUtils.openZip(workspace, dataSource, IntActUpdater.HUMAN_FILE_NAME);
             ZipEntry zipEntry;
             final XmlMapper xmlMapper = XmlMapper.builder().disable(JsonParser.Feature.AUTO_CLOSE_SOURCE).build();
-            // TODO: check negative before parsing
             while ((zipEntry = stream.getNextEntry()) != null) {
-                if (zipEntry.getName().startsWith("human_9606_") && !zipEntry.getName().contains("_negative")) {
+                if (zipEntry.getName().startsWith("human_9606_") && zipEntry.getName().endsWith(".xml")) {
                     if (LOGGER.isInfoEnabled())
                         LOGGER.info("Exporting '" + zipEntry.getName() + "'...");
                     exportEntrySet(graph, xmlMapper.readValue(stream, EntrySet.class));
+                    break;
                 }
             }
         } catch (IOException e) {
@@ -60,7 +66,34 @@ public class IntActGraphExporter extends GraphExporter<IntActDataSource> {
     }
 
     private void exportEntrySet(final Graph graph, final EntrySet entrySet) {
-        // TODO
-        return;
+        final ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
+        for (final Entry entry : entrySet.entries) {
+            try {
+                graph.buildNode().withLabel("Entry").withProperty("source",
+                                                                  objectMapper.writeValueAsString(entry.source))
+                     .withProperty("attributeList", objectMapper.writeValueAsString(entry.attributeList)).withProperty(
+                             "availabilityList", objectMapper.writeValueAsString(entry.availabilityList)).withProperty(
+                             "interactorList", objectMapper.writeValueAsString(entry.interactorList)).withProperty(
+                             "interactionList", objectMapper.writeValueAsString(entry.interactionList)).withProperty(
+                             "experimentList", objectMapper.writeValueAsString(entry.experimentList)).build();
+            } catch (JsonProcessingException e) {
+                e.printStackTrace();
+            }
+            for (final Interactor interactor : entry.interactorList) {
+                getOrCreateBioSource(graph, interactor.organism);
+            }
+        }
+    }
+
+    private Node getOrCreateBioSource(final Graph graph, final BioSource bioSource) {
+        if (bioSource.cellType != null)
+            System.out.println(bioSource.cellType);
+        Node node = graph.findNode("BioSource", "ncbi_tax_id", bioSource.ncbiTaxId);
+        if (node == null) {
+            // TODO: more properties
+            node = graph.addNode("BioSource", "ncbi_tax_id", bioSource.ncbiTaxId);
+        }
+        return node;
     }
 }
