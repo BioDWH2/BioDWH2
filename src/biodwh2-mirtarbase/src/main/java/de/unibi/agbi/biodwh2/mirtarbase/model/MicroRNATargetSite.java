733c98165933ccbf3910bdcd3bab2ba1dc3b4902
Marcel Friedrichs <mfriedrichs@techfak.uni-bielefeld.de>
2022-10-17 11:51:57 +0200

    #75 Add miRTarBase model classes

---
+++ src/biodwh2-mirtarbase/src/main/java/de/unibi/agbi/biodwh2/mirtarbase/model/MicroRNATargetSite.java
@@ -0,0 +1,31 @@
+package de.unibi.agbi.biodwh2.mirtarbase.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+@JsonPropertyOrder({
+        "miRTarBase ID", "miRNA", "Species (miRNA)", "Target Gene", "Target Gene (Entrez ID)", "Species (Target Gene)",
+        "Target Site", "Experiments", "Support Type", "References (PMID)"
+})
+public class MicroRNATargetSite {
+    @JsonProperty("miRTarBase ID")
+    public String miRTarBaseId;
+    @JsonProperty("miRNA")
+    public String miRNA;
+    @JsonProperty("Species (miRNA)")
+    public String speciesMiRNA;
+    @JsonProperty("Target Gene")
+    public String targetGene;
+    @JsonProperty("Target Gene (Entrez ID)")
+    public Long targetGeneEntrezId;
+    @JsonProperty("Species (Target Gene)")
+    public String speciesTargetGene;
+    @JsonProperty("Target Site")
+    public String targetSite;
+    @JsonProperty("Experiments")
+    public String experiments;
+    @JsonProperty("Support Type")
+    public String supportType;
+    @JsonProperty("References (PMID)")
+    public String references;
+}
