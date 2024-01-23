823d38073c8d1f8d8881e0d0e0ec50ac01414e34
Marcel Friedrichs <astror.enales@gmail.com>
2024-01-23 14:47:43 +0100

    Add more ontologies

---
+++ src/biodwh2-ontologies/src/main/java/de/unibi/agbi/biodwh2/ontologies/DiseaseOntologyDataSource.java
@@ -0,0 +1,43 @@
+package de.unibi.agbi.biodwh2.ontologies;
+
+import de.unibi.agbi.biodwh2.core.SingleOBOOntologyDataSource;
+import de.unibi.agbi.biodwh2.core.model.Version;
+import de.unibi.agbi.biodwh2.core.text.License;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+@SuppressWarnings("unused")
+public class DiseaseOntologyDataSource extends SingleOBOOntologyDataSource {
+    private static final Pattern VERSION_PATTERN = Pattern.compile("(\\d{4})-(\\d{2})-(\\d{2})");
+    private static final String FILE_NAME = "doid.obo";
+
+    @Override
+    public String getId() {
+        return "DiseaseOntology";
+    }
+
+    @Override
+    public String getLicense() {
+        return License.CC0_1_0.getName();
+    }
+
+    @Override
+    protected String getDownloadUrl() {
+        return "https://purl.obolibrary.org/obo/" + FILE_NAME;
+    }
+
+    @Override
+    protected String getTargetFileName() {
+        return FILE_NAME;
+    }
+
+    @Override
+    protected Version getVersionFromDataVersionLine(final String dataVersion) {
+        final Matcher matcher = VERSION_PATTERN.matcher(dataVersion);
+        if (matcher.find())
+            return new Version(Integer.parseInt(matcher.group(1)), Integer.parseInt(matcher.group(2)),
+                               Integer.parseInt(matcher.group(3)));
+        return null;
+    }
+}
--- src/biodwh2-ontologies/src/main/java/de/unibi/agbi/biodwh2/ontologies/SymptomOntologyDataSource.java
+++ src/biodwh2-ontologies/src/main/java/de/unibi/agbi/biodwh2/ontologies/SymptomOntologyDataSource.java
@@ -1,4 +1,4 @@
-package de.unibi.agbi.biodwh2.diseaseontology;
+package de.unibi.agbi.biodwh2.ontologies;
 
 import de.unibi.agbi.biodwh2.core.SingleOBOOntologyDataSource;
 import de.unibi.agbi.biodwh2.core.model.Version;
@@ -8,13 +8,13 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 @SuppressWarnings("unused")
-public class DiseaseOntologyDataSource extends SingleOBOOntologyDataSource {
+public class SymptomOntologyDataSource extends SingleOBOOntologyDataSource {
     private static final Pattern VERSION_PATTERN = Pattern.compile("(\\d{4})-(\\d{2})-(\\d{2})");
-    private static final String FILE_NAME = "doid.obo";
+    private static final String FILE_NAME = "symp.obo";
 
     @Override
     public String getId() {
-        return "DiseaseOntology";
+        return "SymptomOntology";
     }
 
     @Override
@@ -24,7 +24,7 @@ public class DiseaseOntologyDataSource extends SingleOBOOntologyDataSource {
 
     @Override
     protected String getDownloadUrl() {
-        return "http://purl.obolibrary.org/obo/" + FILE_NAME;
+        return "https://purl.obolibrary.org/obo/" + FILE_NAME;
     }
 
     @Override
