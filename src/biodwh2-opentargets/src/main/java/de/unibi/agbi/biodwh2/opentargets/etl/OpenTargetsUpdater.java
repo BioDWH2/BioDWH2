0670551c74ee0e2153fa238c5c1aed2aa6b8be00
Marcel Friedrichs <astror.enales@gmail.com>
2024-02-07 12:11:46 +0100

    OpenTargets updater use version number from FTP and download remaining files

--- src/biodwh2-opentargets/src/main/java/de/unibi/agbi/biodwh2/opentargets/etl/OpenTargetsUpdater.java
+++ src/biodwh2-opentargets/src/main/java/de/unibi/agbi/biodwh2/opentargets/etl/OpenTargetsUpdater.java
@@ -16,22 +16,26 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.io.IOException;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
 import java.util.Optional;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class OpenTargetsUpdater extends Updater<OpenTargetsDataSource> {
     private static final Logger LOGGER = LogManager.getLogger(OpenTargetsUpdater.class);
-    // private static final String DATA_VERSION_URL = "https://api.platform.opentargets.org/api/v4/graphql?query={meta{dataVersion{year,month,iteration}}}";
+    private static final String FTP_VERSIONS_URL = "http://ftp.ebi.ac.uk/pub/databases/opentargets/platform/";
     private static final String FTP_BASE_URL = "https://ftp.ebi.ac.uk/pub/databases/opentargets/platform/latest/output/etl/parquet/";
-    private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
     static final String BASELINE_EXPRESSION_FILE_NAME = "baselineExpression.parquet.zip";
     static final String DISEASE_TO_PHENOTYPE_FILE_NAME = "diseaseToPhenotype.parquet.zip";
     static final String DISEASES_FILE_NAME = "diseases.parquet.zip";
     static final String DRUG_WARNINGS_FILE_NAME = "drugWarnings.parquet.zip";
-    static final String EVIDENCE_FILE_NAME = "evidence.parquet.zip";
-    static final String FDA_FILE_NAME = "fda.parquet.zip";
+    static final String FDA_ADR_FILE_NAME = "fda_significantAdverseDrugReactions.parquet.zip";
+    static final String FDA_ATR_FILE_NAME = "fda_significantAdverseTargetReactions.parquet.zip";
+    static final String EPMC_COOCCURRENCES_FILE_NAME = "epmcCooccurrences.parquet.zip";
+    static final String EXPRESSION_SPECIFICITY_FILE_NAME = "expressionSpecificity.parquet.zip";
+    static final String PHARMACOGENOMICS_FILE_NAME = "pharmacogenomics.parquet.zip";
+    static final String TARGET_ESSENTIALITY_FILE_NAME = "targetEssentiality.parquet.zip";
+    static final String TARGET_PRIORITISATION_FILE_NAME = "targetPrioritisation.parquet.zip";
     static final String GO_FILE_NAME = "go.parquet.zip";
     static final String HPO_FILE_NAME = "hpo.parquet.zip";
     static final String INDICATION_FILE_NAME = "indication.parquet.zip";
@@ -43,63 +47,35 @@ public class OpenTargetsUpdater extends Updater<OpenTargetsDataSource> {
     static final String REACTOME_FILE_NAME = "reactome.parquet.zip";
     static final String TARGETS_FILE_NAME = "targets.parquet.zip";
 
+    private static final Pattern VERSION_PATTERN = Pattern.compile("([1-9][0-9]*)\\.([0-9]+)(?:\\.([0-9]+))?/");
+
     public OpenTargetsUpdater(final OpenTargetsDataSource dataSource) {
         super(dataSource);
     }
 
     @Override
     public Version getNewestVersion(final Workspace workspace) throws UpdaterException {
-        final var client = new HTTPFTPClient(FTP_BASE_URL);
+        final var client = new HTTPFTPClient(FTP_VERSIONS_URL);
         try {
-            final var entries = client.listDirectory("diseases");
-            for (final var entry : entries) {
-                if ("_SUCCESS".equals(entry.name)) {
-                    final var dateTime = LocalDateTime.parse(entry.modificationDate, DATE_TIME_FORMATTER);
-                    return new Version(dateTime.getYear(), dateTime.getMonthValue());
+            Version latestVersion = null;
+            for (final var entry : client.listDirectory()) {
+                final Matcher matcher = VERSION_PATTERN.matcher(entry.name);
+                if (matcher.find()) {
+                    final Version version;
+                    if (matcher.groupCount() == 4)
+                        version = new Version(Integer.parseInt(matcher.group(1)), Integer.parseInt(matcher.group(2)),
+                                              Integer.parseInt(matcher.group(3)));
+                    else
+                        version = new Version(Integer.parseInt(matcher.group(1)), Integer.parseInt(matcher.group(2)));
+                    if (latestVersion == null || version.compareTo(latestVersion) > 0)
+                        latestVersion = version;
                 }
             }
+            return latestVersion;
         } catch (IOException e) {
             throw new UpdaterConnectionException(e);
         }
-        // final JsonNode json = loadDataVersionJson();
-        // return parseVersion(json);
-        return null;
     }
-
-    // TODO: once graphql api is fixed
-    // private JsonNode loadDataVersionJson() throws UpdaterException {
-    //     final String source = getWebsiteSource(DATA_VERSION_URL, 5);
-    //     return parseJsonSource(source);
-    // }
-
-    // private JsonNode parseJsonSource(final String source) throws UpdaterMalformedVersionException {
-    //     final ObjectMapper mapper = new ObjectMapper();
-    //     try {
-    //         return mapper.readTree(source);
-    //     } catch (IOException e) {
-    //         throw new UpdaterMalformedVersionException(source, e);
-    //     }
-    // }
-
-    // private Version parseVersion(final JsonNode json) throws UpdaterMalformedVersionException {
-    //     final JsonNode dataNode = json.get("data");
-    //     if (dataNode == null)
-    //         throw new UpdaterMalformedVersionException(json.toString());
-    //     final JsonNode metaNode = dataNode.get("meta");
-    //     if (metaNode == null)
-    //         throw new UpdaterMalformedVersionException(json.toString());
-    //     final JsonNode dataVersionNode = metaNode.get("dataVersion");
-    //     if (dataVersionNode == null)
-    //         throw new UpdaterMalformedVersionException(json.toString());
-    //     final int year = dataVersionNode.get("year").asInt();
-    //     final int month = dataVersionNode.get("month").asInt();
-    //     final int iteration = dataVersionNode.get("iteration").asInt();
-    //     try {
-    //         return new Version(year, month, iteration);
-    //     } catch (NullPointerException | NumberFormatException e) {
-    //         throw new UpdaterMalformedVersionException(dataVersionNode.toString(), e);
-    //     }
-    // }
 
     @Override
     protected boolean tryUpdateFiles(final Workspace workspace) throws UpdaterException {
@@ -108,12 +84,13 @@ public class OpenTargetsUpdater extends Updater<OpenTargetsDataSource> {
             // Not used: AOTFClickhouse, AOTFElasticsearch, errors, ebisearchAssociations, ebisearchEvidence,
             // searchDisease, searchDrug, searchTarget, associationByDatasourceDirect,
             // associationByDatasourceIndirect, associationByDatatypeDirect, associationByDatatypeIndirect,
-            // associationByOverallDirect, associationByOverallIndirect
+            // associationByOverallDirect, associationByOverallIndirect, evidence
             downloadFiles(workspace, client, "baselineExpression");
             downloadFiles(workspace, client, "diseaseToPhenotype");
             downloadFiles(workspace, client, "diseases");
             downloadFiles(workspace, client, "drugWarnings");
-            // TODO: evidence, fda
+            downloadFiles(workspace, client, "fda/significantAdverseDrugReactions");
+            downloadFiles(workspace, client, "fda/significantAdverseTargetReactions");
             downloadFiles(workspace, client, "go");
             downloadFiles(workspace, client, "hpo");
             downloadFiles(workspace, client, "indication");
@@ -125,6 +102,11 @@ public class OpenTargetsUpdater extends Updater<OpenTargetsDataSource> {
             downloadFiles(workspace, client, "mousePhenotypes");
             downloadFiles(workspace, client, "reactome");
             downloadFiles(workspace, client, "targets");
+            downloadFiles(workspace, client, "epmcCooccurrences");
+            downloadFiles(workspace, client, "expressionSpecificity");
+            downloadFiles(workspace, client, "pharmacogenomics");
+            downloadFiles(workspace, client, "targetEssentiality");
+            downloadFiles(workspace, client, "targetPrioritisation");
         } catch (IOException e) {
             throw new UpdaterConnectionException(e);
         }
@@ -134,7 +116,8 @@ public class OpenTargetsUpdater extends Updater<OpenTargetsDataSource> {
     private void downloadFiles(final Workspace workspace, final HTTPFTPClient client,
                                final String directoryName) throws IOException {
         final HTTPFTPClient.Entry[] entries = client.listDirectory(directoryName);
-        final String targetFilePath = dataSource.resolveSourceFilePath(workspace, directoryName + ".parquet.zip");
+        final String fileName = StringUtils.replace(directoryName, "/", "_") + ".parquet.zip";
+        final String targetFilePath = dataSource.resolveSourceFilePath(workspace, fileName);
         final Optional<Integer> maxPartNumber = Arrays.stream(entries).map(e -> e.name).filter(
                 n -> n.startsWith("part-")).map(n -> Integer.parseInt(StringUtils.split(n, '-')[1])).max(
                 Integer::compareTo);
@@ -164,7 +147,9 @@ public class OpenTargetsUpdater extends Updater<OpenTargetsDataSource> {
                 BASELINE_EXPRESSION_FILE_NAME, DISEASE_TO_PHENOTYPE_FILE_NAME, DISEASES_FILE_NAME,
                 DRUG_WARNINGS_FILE_NAME, GO_FILE_NAME, HPO_FILE_NAME, INDICATION_FILE_NAME, INTERACTION_FILE_NAME,
                 INTERACTION_EVIDENCE_FILE_NAME, MECHANISM_OF_ACTION_FILE_NAME, MOLECULE_FILE_NAME,
-                MOUSE_PHENOTYPES_FILE_NAME, REACTOME_FILE_NAME, TARGETS_FILE_NAME
+                MOUSE_PHENOTYPES_FILE_NAME, REACTOME_FILE_NAME, TARGETS_FILE_NAME, FDA_ADR_FILE_NAME, FDA_ATR_FILE_NAME,
+                EPMC_COOCCURRENCES_FILE_NAME, EXPRESSION_SPECIFICITY_FILE_NAME, PHARMACOGENOMICS_FILE_NAME,
+                TARGET_ESSENTIALITY_FILE_NAME, TARGET_PRIORITISATION_FILE_NAME
         };
     }
 }
