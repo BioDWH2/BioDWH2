43f37fad9c900cb4e03e01f11f7672217ca098c3
Marcel Friedrichs <astror.enales@gmail.com>
2025-02-13 10:15:04 +0100

    Fix USDA-PLANTS updater

--- src/biodwh2-usdaplants/src/main/java/de/unibi/agbi/biodwh2/usdaplants/etl/USDAPlantsUpdater.java
+++ src/biodwh2-usdaplants/src/main/java/de/unibi/agbi/biodwh2/usdaplants/etl/USDAPlantsUpdater.java
@@ -6,18 +6,20 @@ import de.unibi.agbi.biodwh2.core.exceptions.UpdaterException;
 import de.unibi.agbi.biodwh2.core.exceptions.UpdaterMalformedVersionException;
 import de.unibi.agbi.biodwh2.core.model.Version;
 import de.unibi.agbi.biodwh2.usdaplants.USDAPlantsDataSource;
-import org.apache.commons.lang3.StringUtils;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class USDAPlantsUpdater extends Updater<USDAPlantsDataSource> {
-    private static final String VERSION_URL = "https://plants.usda.gov/home/release-notes";
+    private static final String VERSION_URL = "https://plants.usda.gov/release-notes";
     private static final String DOWNLOAD_URL = "https://plants.usda.gov/assets/docs/CompletePLANTSList/plantlst.txt";
     private static final String MAIN_JS_URL_PREFIX = "https://plants.usda.gov/";
     static final String PLANT_LIST_FILE_NAME = "plantlst.txt";
 
-    private static final Pattern MAIN_JS_PATTERN = Pattern.compile("src=\"(main\\.[a-f0-9A-F]+\\.js)\"");
+    private static final Pattern MAIN_JS_PATTERN = Pattern.compile("src=\"(main-[a-z0-9A-Z]+\\.js)\"");
+    private static final Pattern CHUNK_JS_PATTERN = Pattern.compile("chunk-[a-z0-9A-Z]+\\.js");
+    private static final Pattern VERSION_PATTERN = Pattern.compile(
+            "version:\"([0-9]+\\.[0-9]+\\.[0-9]+(?:\\.[0-9]+)?)\"");
 
     public USDAPlantsUpdater(final USDAPlantsDataSource dataSource) {
         super(dataSource);
@@ -27,13 +29,19 @@ public class USDAPlantsUpdater extends Updater<USDAPlantsDataSource> {
     public Version getNewestVersion(final Workspace workspace) throws UpdaterException {
         var source = getWebsiteSource(VERSION_URL, 3);
         final Matcher mainJSMatcher = MAIN_JS_PATTERN.matcher(source);
-        if (mainJSMatcher.find())
+        if (mainJSMatcher.find()) {
             source = getWebsiteSource(MAIN_JS_URL_PREFIX + mainJSMatcher.group(1));
-        else
+            final Matcher chunkJSMatcher = CHUNK_JS_PATTERN.matcher(source);
+            while (chunkJSMatcher.find()) {
+                var chunkSource = getWebsiteSource(MAIN_JS_URL_PREFIX + chunkJSMatcher.group(0));
+                final Matcher matcher = VERSION_PATTERN.matcher(chunkSource);
+                if (matcher.find())
+                    return Version.parse(matcher.group(1));
+            }
+        } else {
             throw new UpdaterMalformedVersionException("Failed to retrieve versions");
-        var parts = StringUtils.splitByWholeSeparator(source, "[{version:\"", 2);
-        parts = StringUtils.split(parts[1], "\"", 2);
-        return Version.parse(parts[0].strip());
+        }
+        return null;
     }
 
     @Override
